f83268a9220e6847e9bff6e4988b3bffe2102e27
alinaabdikenova <alinaabdikenova@gmail.com>
2026-03-25 16:18:42 +0500

    practice06

--- practice06/directory_managment/create_list_dirs.py
+++ practice06/directory_managment/create_list_dirs.py
@@ -1,5 +1,16 @@
 import os
 
-os.makedirs("folder/subfolder", exist_ok=True)
 
-print(os.listdir("."))  # список файлов
+os.makedirs("test_folder/subfolder/inner_folder", exist_ok=True)
+print("folders created")
+
+print("\ncontents of test_folder:")
+for item in os.listdir("test_folder"):
+    print(item)
+
+# найти .py файлы только в test_folder
+print("\n.py files:")
+for root, dirs, files in os.walk("test_folder"):
+    for file in files:
+        if file.endswith(".py"):
+            print(os.path.join(root, file))
--- practice06/directory_managment/move_files.py
+++ practice06/directory_managment/move_files.py
@@ -1,3 +1,16 @@
+import os
 import shutil
 
-shutil.move("test.txt", "folder/test.txt")
+# создать папки
+os.makedirs("src", exist_ok=True)
+os.makedirs("dst", exist_ok=True)
+
+# создать файл
+with open("src/file.txt", "w") as f:
+    f.write("hello")
+
+# копировать файл
+shutil.copy("src/file.txt", "dst/file.txt")
+
+# переместить файл
+shutil.move("src/file.txt", "dst/file_moved.txt")
